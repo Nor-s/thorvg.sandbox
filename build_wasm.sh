776273bfa6fe435affde8b81b75a7b9ad14708d4
Sungun No <nors.nsg@gmail.com>
2025-07-27 16:09:18 +0900

    buid: add preload-file link arg

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,5 +1,5 @@
 export PATH="/opt/homebrew/bin:$PATH"
 
-meson setup build/wasm --buildtype=debug --cross-file wasm.ini --default-library=static -Dplatform=web
+meson setup build/wasm --buildtype=release --cross-file wasm.ini --default-library=static -Dplatform=web
 
 meson compile -C build/wasm
